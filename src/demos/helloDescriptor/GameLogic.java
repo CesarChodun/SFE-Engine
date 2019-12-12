8e739f9575a7c20f5e0fab5f94e2f2220c1e292c
Cesar <caesar.chodun@gmail.com>
2019-12-12 20:34:56 +0100

    Update GameLogic.java
    Redirecting logs to standard output

--- src/demos/helloDescriptor/GameLogic.java
+++ src/demos/helloDescriptor/GameLogic.java
@@ -1,11 +1,17 @@
 package demos.helloDescriptor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Semaphore;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import core.Engine;
 import core.EngineTask;
-import demos.helloTriangle.initialization.InitializeEngine;
-import demos.helloTriangle.window.WindowManager;
+import demos.helloDescriptor.initialization.InitializeEngine;
+import demos.helloDescriptor.window.WindowManager;
 
 public class GameLogic implements EngineTask {
 	
@@ -17,6 +23,8 @@ public class GameLogic implements EngineTask {
 
 	@Override
 	public void run() throws AssertionError {
+		redirectLoggers();
+		
 		// Semaphore indicating initialization state
 		Semaphore initialized = new Semaphore(0);
 		
@@ -29,4 +37,21 @@ public class GameLogic implements EngineTask {
 		waitForConfig.start();
 	}
 
+	private static void redirectLoggersToConsole(List<Logger> loggers) {
+		Handler consoleLog = new ConsoleHandler();
+		consoleLog.setLevel(Level.ALL);
+		
+		for (Logger lg : loggers) 
+			lg.addHandler(consoleLog);
+	}
+	
+	private void redirectLoggers() {
+		List<Logger> loggers = new ArrayList<Logger>();
+		Logger logger = Logger.getLogger("");
+		logger.setLevel(Level.ALL);
+		loggers.add(logger);
+		
+		redirectLoggersToConsole(loggers);
+	}
+	
 }
