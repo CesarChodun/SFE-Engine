434952009db9c1452f4484e834a770d0d5ad2ba8
Cesar <caesar.chodun@gmail.com>
2020-01-16 15:09:07 +0100

    Documentation update

--- src/rendering/engine/shader/DescriptorSet.java
+++ src/rendering/engine/shader/DescriptorSet.java
@@ -7,10 +7,24 @@ import java.util.Map;
 import core.resources.Destroyable;
 import core.result.VulkanException;
 
+/**
+ * Vulkan descriptor set.
+ * 
+ * @author Cezary Chodun
+ * @since 10.01.2020
+ */
 public abstract class DescriptorSet implements Destroyable{
 
+	/** A map with the descriptors contained within the set. */
 	private Map<String, Descriptor> descriptors = Collections.synchronizedMap(new HashMap<String, Descriptor>());
 	
+	/**
+	 * Tells whether all of the descriptors within
+	 * the set are up to date.
+	 * 
+	 * @return true if all of the descriptors are up to date 
+	 * 		and false otherwise.
+	 */
 	public boolean isUpToDate() {
 		for (Map.Entry<String, Descriptor> i : descriptors.entrySet())
 			if (!i.getValue().isUpToDate())
@@ -18,17 +32,41 @@ public abstract class DescriptorSet implements Destroyable{
 		return true;
 	}
 	
+	/**
+	 * Updates all of the descriptors within the set.
+	 * 
+	 * @throws VulkanException 
+	 */
 	public void update() throws VulkanException {
 		for (Map.Entry<String, Descriptor> i : descriptors.entrySet())
 			i.getValue().update();
 	}
 
+	/**
+	 * Returns vulkan descriptor set.
+	 * 
+	 * @return handle of the descriptor set.
+	 */
 	public abstract long getDescriptorSet();
 	
+	/**
+	 * Adds a descriptor to the set.
+	 * 
+	 * @param name			descriptor name.
+	 * @param descriptor	the descriptor.
+	 * @return	the previous descriptor associated with the name or null
+	 * 		if there is no such a descriptor.
+	 */
 	public Descriptor addDescriptor(String name, Descriptor descriptor) {
 		return descriptors.put(name, descriptor);
 	}
 	
+	/**
+	 * Gets a descriptor with specified name.
+	 * 
+	 * @param name	descriptor name.
+	 * @return	the descriptor.
+	 */
 	public Descriptor get(String name) {
 		return descriptors.get(name);
 	}
