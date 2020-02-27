94ae7769ded029aae40fc4fb370a32efdcd88f9c
Cesar <caesar.chodun@gmail.com>
2020-02-27 20:28:10 +0100

    Engine work synchronization

---
+++ src/core/synchronization/Dependency.java
@@ -0,0 +1,25 @@
+package core.synchronization;
+
+/**
+ * 
+ * @author Cezary Chodun
+ * @since 26.02.2020
+ */
+public interface Dependency {
+
+	/**
+	 * Adds a smart queue to the list, so that it can be updated
+	 * when the dependency will be released.
+	 * 
+	 * @param queue	the queue.
+	 */
+	public void addSmartQueue(SmartWaitQueue queue);
+	
+	/**
+	 * Tells whether the dependency was satisfied.
+	 * 
+	 * @return	true if the dependency was satisfied
+	 * 		and false otherwise.
+	 */
+	public boolean isReleased();
+}
