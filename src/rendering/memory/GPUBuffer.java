ebdd8b13e567c306a0b1374b1446f9c69fb9684e
Cesar <caesar.chodun@gmail.com>
2020-03-06 17:17:37 +0100

    Update GPUBuffer.java

--- src/rendering/memory/GPUBuffer.java
+++ src/rendering/memory/GPUBuffer.java
@@ -7,8 +7,11 @@ package rendering.memory;
  * @since 10.01.2020
  */
 public class GPUBuffer {
+    /** Vulkan memory buffer. */
     public long buffer;
+    /** Allocated memory address. */
     public long memory;
+    /** Size of the buffer. */
     public long allocationSize;
 
     public GPUBuffer() {}
