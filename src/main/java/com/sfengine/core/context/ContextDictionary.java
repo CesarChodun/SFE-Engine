660988935d23abc6c4c73471b4a794bdc275a79d
Cesar <caesar.chodun@gmail.com>
2020-06-04 13:09:06 +0200

    Update ContextDictionary.java

--- src/main/java/com/sfengine/core/context/ContextDictionary.java
+++ src/main/java/com/sfengine/core/context/ContextDictionary.java
@@ -2,50 +2,35 @@ package com.sfengine.core.context;
 
 import com.sfengine.core.synchronization.Dependency;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class ContextDictionary {
 
-    private Map<String, String> entries = new HashMap<>();
-    private ContextDictionary parent;
+    private final Map<String, String> entries = Collections.synchronizedMap(new HashMap<>());
 
     public ContextDictionary() {}
 
     public ContextDictionary(ContextDictionary dict) {
-        parent = dict;
+        addAll(dict);
     }
 
     public boolean contains(String factoryIdentifier) {
-        synchronized (entries) {
-            if (entries.containsKey(factoryIdentifier))
-                return true;
-        }
-
-        if (parent != null)
-            return parent.contains(factoryIdentifier);
-
-        return false;
+        return entries.containsKey(factoryIdentifier);
     }
 
     public String get(String factoryIdentifier) {
-        String out;
-
-        synchronized(entries) {
-            out = entries.get(factoryIdentifier);
-        }
-
-        if (out == null && parent != null)
-            return parent.get(factoryIdentifier);
-
-        return out;
+        return entries.get(factoryIdentifier);
     }
 
     public <T extends Context> String put(T context) {
-        synchronized (entries) {
-            return entries.put(context.getFactoryIdentifier(), context.getName());
-        }
+        return entries.put(context.getFactoryIdentifier(), context.getName());
+    }
+
+    public void addAll(ContextDictionary dict) {
+        entries.putAll(dict.entries);
     }
 
 }
