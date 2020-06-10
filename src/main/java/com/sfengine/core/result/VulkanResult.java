8d7573be71438790720c8bc7bd2fa1122ccea375
Cesar <caesar.chodun@gmail.com>
2020-06-10 12:17:04 +0200

    Update VulkanResult.java

--- src/main/java/com/sfengine/core/result/VulkanResult.java
+++ src/main/java/com/sfengine/core/result/VulkanResult.java
@@ -13,7 +13,7 @@ public class VulkanResult {
      * And based on that either throws an error, an exception, or does nothing.
      *
      * @param vkResult The Vulkan result.
-     * @param message Message in case of error/exception.
+     * @param message Message in case of an error/exception.
      * @throws VulkanException In case that vkResult represents an exception message.
      */
     public static void validate(int vkResult, String message) throws VulkanException {
@@ -23,6 +23,20 @@ public class VulkanResult {
 
         if (vkResult > 0) {
             throw new VulkanException(vkResult, message);
+        }
+
+        throw new VulkanError(vkResult, message);
+    }
+
+    /**.
+     * Throws vulkan error if the vkResult ID is not VK_SUCCESS.
+     *
+     * @param vkResult The Vulkan result.
+     * @param message Message in case of an error.
+     */
+    public static void assertValidate(int vkResult, String message) {
+        if (vkResult == 0) {
+            return;
         }
 
         throw new VulkanError(vkResult, message);
