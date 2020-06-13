e282179be50126d6470326aaf6e78c3332bc50b9
Cesar <caesar.chodun@gmail.com>
2020-06-13 11:43:53 +0200

    Update BasicRenderJobContext.java

--- src/main/java/com/sfengine/components/contexts/renderjob/BasicRenderJobContext.java
+++ src/main/java/com/sfengine/components/contexts/renderjob/BasicRenderJobContext.java
@@ -1,11 +1,14 @@
 package com.sfengine.components.contexts.renderjob;
 
+import com.sfengine.core.context.ContextDictionary;
+import com.sfengine.core.context.ContextUtil;
 import com.sfengine.core.context.renderjob.RenderJobContext;
-import com.sfengine.core.rendering.DefaultRenderer;
+import com.sfengine.core.context.swapchain.SwapchainContext;
 import com.sfengine.core.rendering.factories.CommandBufferFactory;
 import com.sfengine.core.rendering.recording.RenderJob;
 import com.sfengine.core.synchronization.Dependency;
 import com.sfengine.core.synchronization.DependencyFence;
+import org.jetbrains.annotations.Nullable;
 import org.lwjgl.vulkan.VkCommandBuffer;
 
 import java.util.HashMap;
@@ -21,12 +24,17 @@ public class BasicRenderJobContext implements RenderJobContext {
 
     private final Map<Long, RenderJob> cmds = new HashMap<>();
 
+    private ContextDictionary dict;
+    private Runnable update;
+
     private static class BasicRenderJob implements RenderJob {
 
         private VkCommandBuffer cmd;
+        private Runnable update;
 
-        public BasicRenderJob(VkCommandBuffer cmd) {
+        public BasicRenderJob(VkCommandBuffer cmd, @Nullable Runnable update) {
             this.cmd = cmd;
+            this.update = update;
         }
 
         @Override
@@ -36,23 +44,27 @@ public class BasicRenderJobContext implements RenderJobContext {
 
         @Override
         public void performUpdate() {
-
+            if (update != null)
+                update.run();
         }
     }
 
-    public BasicRenderJobContext(String name, String factoryIdentifier, CommandBufferFactory cmdFactory) {
+    public BasicRenderJobContext(String name, ContextDictionary dict, String factoryIdentifier, CommandBufferFactory cmdFactory, @Nullable Runnable update) {
         this.name = name;
         this.factoryIdentifier = factoryIdentifier;
         this.cmdFactory = cmdFactory;
         created.release();
+        this.dict = dict;
+        this.update = update;
     }
 
     @Override
     public void recreateJobs(long... frameBuffers) {
-        VkCommandBuffer[] bufs = cmdFactory.createCmdBuffers(DefaultRenderer.width, DefaultRenderer.height, frameBuffers);
+        SwapchainContext swContext = ContextUtil.getSwapchain(dict);
+        VkCommandBuffer[] bufs = cmdFactory.createCmdBuffers(swContext.info().imageExtent().width(), swContext.info().imageExtent().height(), frameBuffers);
 
         for (int i = 0; i < frameBuffers.length; i++)
-            cmds.put(frameBuffers[i], new BasicRenderJob(bufs[i]));
+            cmds.put(frameBuffers[i], new BasicRenderJob(bufs[i], update));
     }
 
     @Override
