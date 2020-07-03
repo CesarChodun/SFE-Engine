825e24cae0531df27d8ca2078e4bd9bf6e301610
Cesar <caesar.chodun@gmail.com>
2020-07-03 21:55:09 +0200

    Update Dependable.java

--- src/main/java/com/sfengine/core/synchronization/Dependable.java
+++ src/main/java/com/sfengine/core/synchronization/Dependable.java
@@ -2,5 +2,11 @@ package com.sfengine.core.synchronization;
 
 public interface Dependable {
 
+    /**
+     * Returns a <code>Dependency</> object that will
+     * be released when initialization of <b><code>this</></> object will be complete.
+     *
+     * @return <code>Dependency</> semaphore.
+     */
     Dependency getDependency();
 }
